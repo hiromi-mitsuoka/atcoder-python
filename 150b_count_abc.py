20b966b00f62dbfd0842974271b565f1d7972898
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-06-01 15:13:04 +0900

    Finish s150_count_abc

---
+++ 150b_count_abc.py
@@ -0,0 +1,17 @@
+N = int(input())
+S = str(input())
+
+
+# count = 0
+
+# for i in range(N-2):
+#   if S[i] == 'A':
+#     if S[i+1] == 'B':
+#       if S[i+2] == 'C':
+#         count += 1
+
+# print(count)
+
+# https://hibiki-press.tech/python/count/103
+# NOTE: 'ABC'の出現回数を数える
+print(S.count('ABC'))
