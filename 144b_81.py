fcd8ac0ca898fa7d86abb9733ac30d3ea6932c12
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-06-01 14:52:47 +0900

    Add comments

--- 144b_81.py
+++ 144b_81.py
@@ -1,11 +1,12 @@
 N = int(input())
 
 list = []
-
+# NOTE: 最初に候補を全て計算しておく
 for i in range(1, 10):
   for j in range(1, 10):
     list.append(i*j)
 
+# NOTE: 配列内にNがあるかどうか
 result = N in list
 if result:
   print("Yes")
