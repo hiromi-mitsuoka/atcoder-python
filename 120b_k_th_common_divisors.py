714a64dc3291835fbfa710212289a7ec9c6f1b0f
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-06-07 08:54:26 +0900

    Another answer of 120_b_

--- 120b_k_th_common_divisors.py
+++ 120b_k_th_common_divisors.py
@@ -1,21 +1,29 @@
-def divisor(n):
-  i = 1
-  table = []
-  while i * i <= n:
-    if n % i == 0:
-      table.append(i)
-      table.append(n//i)
-    i += 1
-  table = list(set(table)) # set(): 重複取り除き，set(集合) に変換
-  table.sort()
-  return table
+# def divisor(n):
+#   i = 1
+#   table = []
+#   while i * i <= n:
+#     if n % i == 0:
+#       table.append(i)
+#       table.append(n//i)
+#     i += 1
+#   table = list(set(table)) # set(): 重複取り除き，set(集合) に変換
+#   table.sort()
+#   return table
 
 a, b, k = map(int, input().split())
 
-set_a = set(divisor(a))
-set_b = set(divisor(b))
+# set_a = set(divisor(a))
+# set_b = set(divisor(b))
 
-duplication = list(set_a & set_b)
-duplication.sort(reverse=True)
-# print(duplication)
-print(duplication[k-1])
+# duplication = list(set_a & set_b)
+# duplication.sort(reverse=True)
+# # print(duplication)
+# print(duplication[k-1])
+
+list = []
+for i in range(100, 0, -1):
+  if a % i == 0 and b % i == 0:
+    list.append(i)
+
+# print(list)
+print(list[k-1])
